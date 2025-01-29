78a746494d43e32e5e2f747dd28af58ef4bf22bf
moonyango <mosesadrian825@gmail.com>
2025-01-29 04:37:41 +0300

    game constants

---
+++ js/constants.js
@@ -0,0 +1,19 @@
+// game constants
+export const GAME_SPEED = 1000;
+export const MAX_LEVEL = 10;
+export const STARTING_LIVES = 3;
+export const HEART_EMOJI = "❤️";
+export const FPS = 60;
+export const FRAME_TIME = 1000 / FPS;
+export const COLORS = [
+  "cyan",
+  "blue",
+  "orange",
+  "yellow",
+  "green",
+  "purple",
+  "red",
+];
+export const BOARD_HEIGHT = 20;
+export const BOARD_WIDTH = 10;
+export const CENTER = Math.floor(BOARD_WIDTH / 2) - 1;
