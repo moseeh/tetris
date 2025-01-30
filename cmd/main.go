1fd3637e10aae1bc4f4db9325beb71d3a6b48dd5
aosindo <andyovvo8@gmail.com>
2025-01-30 13:10:43 +0300

    chore: added main func

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", HomeHandler)
+	http.HandleFunc("/scores", PostScoresHandler)
+	http.HandleFunc("/return", ReturnHandler)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static")))) // Serve static files from the./static directory.
+
+	fmt.Println("Server started http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
