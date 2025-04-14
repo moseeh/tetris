7112171085b1848a427e9154c4e18c6caf4569bb
moonyango <mosesadrian825@gmail.com>
2025-04-14 06:05:36 +0300

    feat: initial tilemaps

---
+++ static/js/tilemaps/backgroundCelestial.js
@@ -0,0 +1,12 @@
+export const BackgroundCelestial = {
+    name: "celestial",
+    tileSize: 64,
+    columns: 8,
+    rows: 6,
+    grid: [
+      1, 2, 2, 2, 3, 2, 2, 4, 2, 0, 0, 0, 0, 0, 0, 2, 2, 0, 5, 0, 5, 0, 0, 2, 2,
+      0, 0, 0, 0, 0, 0, 2, 4, 2, 0, 0, 0, 0, 2, 1, 3, 2, 2, 2, 2, 2, 2, 4,
+    ],
+    tilesetCols: 4,
+  };
+  
---
+++ static/js/tilemaps/backgroundReborn.js
@@ -0,0 +1,11 @@
+export const BackgroundReborn = {
+  name: "reborn",
+  tileSize: 64,
+  columns: 8,
+  rows: 5,
+  grid: [
+    5, 5, 5, 5, 5, 5, 5, 5, 5, 0, 0, 0, 0, 0, 0, 5, 5, 0, 3, 0, 3, 0, 0, 5, 5,
+    0, 0, 0, 0, 0, 0, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+  ],
+  tilesetCols: 3,
+};
